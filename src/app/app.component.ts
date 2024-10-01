206512a563bceb18b90ec953a398b892564dc058
Gilles <quodbach.gilles@gmail.com>
2024-10-01 12:51:59 +0200

    apropos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MaterialComponents } from './material/material.module';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { AproposComponent } from './components/apropos/apropos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MaterialComponents, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    MaterialComponents,
+    HeaderComponent,
+    HomeComponent,
+    AproposComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
