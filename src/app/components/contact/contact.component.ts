efd12ac5d60beff3a0f8ccac0386974c778a710c
Gilles <quodbach.gilles@gmail.com>
2024-10-02 11:42:50 +0200

    ajout svg icon

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MaterialComponents } from '../../material/material.module';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact',
@@ -8,4 +10,18 @@ import { MaterialComponents } from '../../material/material.module';
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
-export class ContactComponent {}
+export class ContactComponent {
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'facebook_icon',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('facebook.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'instagram_icon',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('instagram.svg')
+    );
+  }
+}
