b4fdbdb85f39b26d2db0dcad2ee9b06863f18556
Gilles <quodbach.gilles@gmail.com>
2024-10-03 15:48:07 +0200

    still the end

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,26 @@
+import { MaterialComponents } from './../../material/material.module';
 import { Component } from '@angular/core';
-
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [MaterialComponents],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
 export class FooterComponent {
-
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'facebook_icon',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('facebook.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'instagram_icon',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('instagram.svg')
+    );
+  }
 }
