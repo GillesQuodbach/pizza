3c1873c2e9b40c28bfe3101e494b4b0573923ac0
Gilles <quodbach.gilles@gmail.com>
2024-10-04 12:51:24 +0200

    responsive fix

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,5 +1,5 @@
 import { PizzaService } from './../../services/pizza.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { MaterialComponents } from '../../material/material.module';
 import { CommonModule } from '@angular/common';
 @Component({
@@ -12,9 +12,41 @@ import { CommonModule } from '@angular/common';
 export class CardComponent implements OnInit {
   pizzas: any[] = [];
 
-  constructor(private pizzaService: PizzaService) {}
+  gridCols: any;
+  tileHeight: string;
+
+  constructor(private pizzaService: PizzaService) {
+    this.setGridCols();
+    this.tileHeight = '2.6:1';
+  }
 
   ngOnInit(): void {
     this.pizzas = this.pizzaService.getPizzas().pizzas;
+  }
+
+  setGridCols() {
+    const width = window.innerWidth;
+
+    if (width >= 1390) {
+      this.gridCols = 2;
+      this.tileHeight = '2:1';
+    } else if (width >= 1170) {
+      this.gridCols = 2;
+      this.tileHeight = '1.5:1';
+    } else if (width >= 725) {
+      this.gridCols = 1;
+      this.tileHeight = '350px';
+    } else if (width >= 650) {
+      this.gridCols = 1;
+      this.tileHeight = '400px';
+    } else {
+      this.gridCols = 1;
+      this.tileHeight = '400px';
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.setGridCols();
   }
 }
